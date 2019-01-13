19bc3eefd353b175e544de564f7f3059d547f98d
Stanley Lee <stanleylee91776@gmail.com>
2019-01-13 08:09:04 -0800

    wfinal

--- main.py
+++ main.py
@@ -20,54 +20,57 @@ def home():
     return render_template('index.html')
 
 
-# SAVE_PATH = "C:/Users/Stanley/Desktop/SBHacks-2019/videos"
-# link = "https://www.youtube.com/watch?v=kt09W17edIk"
-# yt = YouTube(link)
-# stream = yt.streams.filter(file_extension='mp4').first()
-# stream.download(SAVE_PATH)
-# title = yt.title + ".mp4"
-# title = title.replace("|", "")
-# title = title.replace(":", "")
+@app.route('/results')
+def results():
+    link = request.args['inputname']
+    SAVE_PATH = "C:/Users/Stanley/Desktop/SBHacks-2019/videos"
+    yt = YouTube(link)
+    stream = yt.streams.filter(file_extension='mp4').first()
+    stream.download(SAVE_PATH)
+    title = yt.title + ".mp4"
+    title = title.replace("|", "")
+    title = title.replace(":", "")
 
-# arraylink = link.split("=")
-# video_id = arraylink[1]
-# download = Download()
-# captions = download.get_captions(video_id)
-# sentiment = TextBlob(captions)
-# captions = captions.replace("[Music]", "")
-# captions = captions.replace("  ", ". ")
-# print(captions)
-# print("Sentiment Score: ", sentiment.sentiment.polarity)
-# print(title)
+    arraylink = link.split("=")
+    video_id = arraylink[1]
+    download = Download()
+    captions = download.get_captions(video_id)
+    sentiment = TextBlob(captions)
+    captions = captions.replace("[Music]", "")
+    captions = captions.replace("  ", ". ")
+    print(captions)
+    print("Sentiment Score: ", sentiment.sentiment.polarity)
+    print(title)
 
-# m = clarifail.public_models.general_model
-# try:
-#     response = m.predict_by_filename("C:/Users/Stanley/Desktop/SBHacks-2019/videos/" + title,
-#                                      is_video=True,
-#                                      sample_ms=10000)
-# except ApiError as e:
-#     print('Error status code: %d' % e.error_code)
-#     print('Error description: %s' % e.error_desc)
-#     if e.error_details:
-#         print('Error details: %s' % e.error_details)
-#     exit(1)
+    m = clarifail.public_models.general_model
+    try:
+        response = m.predict_by_filename("C:/Users/Stanley/Desktop/SBHacks-2019/videos/" + title,
+                                         is_video=True,
+                                         sample_ms=10000)
+    except ApiError as e:
+        print('Error status code: %d' % e.error_code)
+        print('Error description: %s' % e.error_desc)
+        if e.error_details:
+            print('Error details: %s' % e.error_details)
+        exit(1)
 
-# total_concepts = {}
-# frames = response['outputs'][0]['data']['frames']
-# for frame in frames:
-#     print('Concepts in frame at time: %d ms' % frame['frame_info']['time'])
-#     for concept in frame['data']['concepts']:
-#         print(' %s %f' % (concept['name'], concept['value']))
-#         if concept['name'] in total_concepts.keys():
-#             total_concepts[concept['name']] += concept['value']
-#         else:
-#             total_concepts[concept['name']] = concept['value']
+    total_concepts = {}
+    frames = response['outputs'][0]['data']['frames']
+    for frame in frames:
+        print('Concepts in frame at time: %d ms' % frame['frame_info']['time'])
+        for concept in frame['data']['concepts']:
+            print(' %s %f' % (concept['name'], concept['value']))
+            if concept['name'] in total_concepts.keys():
+                total_concepts[concept['name']] += concept['value']
+            else:
+                total_concepts[concept['name']] = concept['value']
 
-# final = sorted(total_concepts.items(), key=operator.itemgetter(1), reverse=True)
+    final = sorted(total_concepts.items(), key=operator.itemgetter(1), reverse=True)
 
-# for x in range(5):
-#     print(final[x])
+    for x in range(5):
+        print(final[x])
+    return render_template('results.html', content1=captions,)
 
 
 if __name__ == "__main__":
-    app.run(port=5021, debug=True)
+    app.run(port=5027, debug=True)
